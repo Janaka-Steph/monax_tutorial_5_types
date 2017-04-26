9a4491f106521f9c8e07b36241003314a2b541f9
Janaka-Steph <rochestephane@yahoo.fr>
2017-04-26 16:29:57 +0200

    register contracts in deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,30 @@
-var Bank = artifacts.require("./Bank.sol");
-var BankDb = artifacts.require("./BankDb.sol");
-var ContractProvider = artifacts.require("./ContractProvider.sol");
-var Doug = artifacts.require("./Doug.sol");
-var DougEnabled = artifacts.require("./DougEnabled.sol");
-var FundManager = artifacts.require("./FundManager.sol");
-var FundManagerEnabled = artifacts.require("./FundManagerEnabled.sol");
-var Permissions = artifacts.require("./Permissions.sol");
-var PermissionsDb = artifacts.require("./PermissionsDb.sol");
+var Bank = artifacts.require("./Bank.sol")
+var BankDb = artifacts.require("./BankDb.sol")
+var ContractProvider = artifacts.require("./ContractProvider.sol")
+var Doug = artifacts.require("./Doug.sol")
+var DougEnabled = artifacts.require("./DougEnabled.sol")
+var FundManager = artifacts.require("./FundManager.sol")
+var FundManagerEnabled = artifacts.require("./FundManagerEnabled.sol")
+var Permissions = artifacts.require("./Permissions.sol")
+var PermissionsDb = artifacts.require("./PermissionsDb.sol")
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(Doug)
     .then(() => {
-        deployer.deploy(Bank);
-        deployer.deploy(BankDb);
-        deployer.deploy(ContractProvider);
-        deployer.deploy(DougEnabled);
-        deployer.deploy(FundManager);
-        deployer.deploy(FundManagerEnabled);
-        deployer.deploy(Permissions);
-        deployer.deploy(PermissionsDb);
-  })
-};
+      deployer.deploy(ContractProvider)
+      deployer.deploy(DougEnabled)
+      deployer.deploy(FundManagerEnabled)
+
+      deployer.deploy(Bank)
+        .then(() => Doug.at(Doug.address).addContract('bank', Bank.address))
+      deployer.deploy(BankDb)
+        .then(() => Doug.at(Doug.address).addContract('bankdb', BankDb.address))
+      deployer.deploy(FundManager)
+        .then(() => Doug.at(Doug.address).addContract('fm', FundManager.address))
+        .then(() => Doug.at(Doug.address).addContract('bank', Bank.address))
+      deployer.deploy(Permissions)
+        .then(() => Doug.at(Doug.address).addContract('perms', Permissions.address))
+      deployer.deploy(PermissionsDb)
+        .then(() => Doug.at(Doug.address).addContract('permsdb', PermissionsDb.address))
+    })
+}
